4b75a884735ac2017d5942fb702bef59e0f2d145
carlosp-11 <carlosponce11@gmail.com>
2024-12-21 01:21:29 +0000

    feat: dropdown arrow component

---
+++ tu_web/src/components/DropdownArrow.jsx
@@ -0,0 +1,15 @@
+export const DropdownArrow =() => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+        >
+            <path
+                d="M1.5 3.5a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 .354.854l-6 6a.5.5 0 0 1-.708 0l-6-6A.5.5 0 0 1 1.5 3.5z"
+                />
+        </svg>
+    );
+}

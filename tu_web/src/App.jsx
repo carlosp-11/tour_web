ed3c784d9e37904068c2b28fa65649883204e14e
carlosp-11 <carlosponce11@gmail.com>
2024-12-22 00:22:02 +0000

    chore: refactor

--- tu_web/src/App.jsx
+++ tu_web/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from './pages/home'
 import ScrollToTop from "./components/scrollToTop"
 import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,16 +13,15 @@ import './App.css'
 function App() {
   const basename = "";
   
-
   return (
-    <div className="bg-secondary vh-100">
+    <div className="bg-info-subtle vh-100">
       <BrowserRouter basename={basename}>
         <ScrollToTop>
             <Navbar />
             <Routes>
               <Route element={<Home />} path="/" />
             </Routes>
-            {/*<Footer />*/}
+            <Footer />
         </ScrollToTop>
       </BrowserRouter>
     </div>
--- tu_web/src/components/Navbar.jsx
+++ tu_web/src/components/Navbar.jsx
@@ -31,92 +31,90 @@ export const Navbar =(props)=> {
       }, []);
 
     return (
-        <div className="w-100 bg-white">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid mx-4">
-                    <a className="navbar-brand" href="#">
-                        <img style={{height: "4rem"}} src={Logo} alt="logo turismo urbano" />
-                    </a>
-                    <button className="navbar-toggler" 
-                        type="button" 
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarSupportedContent" 
-                        aria-controls="navbarSupportedContent" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item mx-1">
-                                <a className="nav-link" href="#">Comprar</a>
-                            </li>
-                            <li className="nav-item dropdown mx-1"  ref={alquileresRef}>
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                    onClick={() => toggleDropdown("alquileres")}
-                                >
-                                    Alquileres
-                                    <span className={`dropdown-arrow ${openDropdown === "alquileres" ? "rotate" : ""}`}>
-                                        <DropdownArrow/>
-                                    </span>
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <DropdownLink text="Corta temporada" subtitle="1 - 6 meses" linkFunction={toggleDropdown}/>
-                                    </li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li>
-                                        <DropdownLink text="Larga temporada" subtitle="> 1 año" linkFunction={toggleDropdown}/>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item mx-1">
-                                <a className="nav-link" href="#">Vender</a>
-                            </li>
-                            <li className="nav-item dropdown mx-1"  ref={serviciosRef}>
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                    onClick={() => toggleDropdown("servicios")}
-                                >
-                                    Servicios
-                                    <span className={`dropdown-arrow ${openDropdown === "servicios" ? "rotate" : ""}`}>
-                                       <DropdownArrow/>
-                                    </span>
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <DropdownLink text="Asesoría" linkFunction={toggleDropdown}/>
-                                    </li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li>
-                                        <DropdownLink text="Gestión" linkFunction={toggleDropdown}/>
-                                    </li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li>
-                                        <DropdownLink text="Seguros" linkFunction={toggleDropdown}/>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item mx-1">
-                                <a className="nav-link" href="#"> Nosotros </a>
-                            </li>
-                            <li className='mx-4'>
-                                <ButtonImage text='Contacto' icon= 'mail' />
-                            </li>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid mx-4">
+                <a className="navbar-brand" href="#">
+                    <img style={{height: "4rem"}} src={Logo} alt="logo turismo urbano" />
+                </a>
+                <button className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-1">
+                            <a className="nav-link" href="#">Comprar</a>
+                        </li>
+                        <li className="nav-item dropdown mx-1"  ref={alquileresRef}>
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                onClick={() => toggleDropdown("alquileres")}
+                            >
+                                Alquileres
+                                <span className={`dropdown-arrow ${openDropdown === "alquileres" ? "rotate" : ""}`}>
+                                    <DropdownArrow/>
+                                </span>
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <DropdownLink text="Corta temporada" subtitle="1 - 6 meses" linkFunction={toggleDropdown}/>
+                                </li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li>
+                                    <DropdownLink text="Larga temporada" subtitle="> 1 año" linkFunction={toggleDropdown}/>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="nav-item mx-1">
+                            <a className="nav-link" href="#">Vender</a>
+                        </li>
+                        <li className="nav-item dropdown mx-1"  ref={serviciosRef}>
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                onClick={() => toggleDropdown("servicios")}
+                            >
+                                Servicios
+                                <span className={`dropdown-arrow ${openDropdown === "servicios" ? "rotate" : ""}`}>
+                                    <DropdownArrow/>
+                                </span>
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <DropdownLink text="Asesoría" linkFunction={toggleDropdown}/>
+                                </li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li>
+                                    <DropdownLink text="Gestión" linkFunction={toggleDropdown}/>
+                                </li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li>
+                                    <DropdownLink text="Seguros" linkFunction={toggleDropdown}/>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="nav-item mx-1">
+                            <a className="nav-link" href="#"> Nosotros </a>
+                        </li>
+                        <li className='mx-4'>
+                            <ButtonImage text='Contacto' icon= 'mail' />
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }

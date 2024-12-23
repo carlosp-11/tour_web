0cd8f3a75aa21f68d7f3ea080c05ddeb2704233b
carlosp-11 <carlosponce11@gmail.com>
2024-12-23 00:20:25 +0000

    fix: including ws option on button

--- tu_web/src/components/ButtonImage.jsx
+++ tu_web/src/components/ButtonImage.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react';
-import Mail from '../assets/icons/envelope-regular.png'
-import Scope from '../assets/icons/scope.png'
+import Mail from '../assets/icons/envelope-regular.png';
+import Scope from '../assets/icons/scope.png';
+import WSBusiness from '../assets/icons/small-ws-bussiness.png';
 
 export const ButtonImage = (props) => {
-    const currentIcon = props.icon === 'mail' ? Mail : Scope;
+    const currentIcon = props.icon === 'mail' ? Mail : props.icon === 'scope' ? Scope : WSBusiness;
 
     return(   
-        <button className="btn btn-primary" type="button">
+        <button className={`btn btn-primary ${ props.icon === 'mail' ? "btn-primary": props.icon === 'scope' ? "btn-secondary": "btn-danger" }`} type="button">
             {props.text}
             <span className="ms-2">
-                <img style={currentIcon=== Mail? {height: "0.8rem"}: {height: "1.1rem"}} src={currentIcon} alt="icono mail" />
+                <img style={currentIcon=== Mail? {height: "0.8rem"}: currentIcon=== Scope? {height: "1.1rem"}:{height: "2.3rem", marginTop: -15, marginBottom: -10} } 
+                    src={currentIcon} alt="icono mail" 
+                />
             </span>
         </button>
     );

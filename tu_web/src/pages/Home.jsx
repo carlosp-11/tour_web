bd1dea21e037c69e8abea1605118f31966ae35e0
carlosp-11 <carlosponce11@gmail.com>
2024-12-25 18:26:42 +0000

    feat: including jumbotron on home

--- tu_web/src/pages/Home.jsx
+++ tu_web/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { ReviewCarrousel } from "../components/ReviewCarrousel";
 import { ServicesJumbotron } from "../components/ServicesJumbotron";
 import { FinalJumbotron } from "../components/FinalJumbotron";
+import { FirstJumbotron } from "../components/Firstjumbotron";
 
 export const Home = () => {
     return (
         <div className="">
+            <FirstJumbotron/>
             <ServicesJumbotron/>
             <ReviewCarrousel/>
             <FinalJumbotron/>

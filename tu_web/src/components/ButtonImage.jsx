8d8052635555ca0f9104ada02dfa8a15c71f1a57
carlosp-11 <carlosponce11@gmail.com>
2025-01-05 22:24:03 +0000

    fix: button

--- tu_web/src/components/ButtonImage.jsx
+++ tu_web/src/components/ButtonImage.jsx
@@ -1,15 +1,24 @@
 import Mail from '../assets/icons/envelope-regular.png';
-import Scope from '../assets/icons/scope.png';
 import WSBusiness from '../assets/icons/small-ws-bussiness.png';
+import ScopeSVG from '../assets/svg/scope.svg';
 
 export const ButtonImage = (props) => {
-    const currentIcon = props.icon === 'mail' ? Mail : props.icon === 'scope' ? Scope : WSBusiness;
+    const currentIcon = props.icon === 'mail' ? Mail : props.icon === 'scope' ? ScopeSVG : WSBusiness;
+    const openWhatsApp = () => {
+        const phoneNumber = '1234567890'; // Reemplaza con el número de teléfono (incluyendo el código del país, pero sin signos como '+').
+        const message = encodeURIComponent('¡Hola! Quiero más información.'); // Opcional: mensaje predefinido.
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+    
+        window.open(whatsappUrl, '_blank'); // Abre WhatsApp en una nueva pestaña o aplicación móvil.
+      };
 
     return(   
-        <button className={`btn btn-primary ${ props.icon === 'mail' ? "btn-primary": props.icon === 'scope' ? "btn-secondary": "btn-danger" }`} type="button">
-            {props.text}
-            <span className="ms-2">
-                <img style={currentIcon=== Mail? {height: "0.8rem"}: currentIcon=== Scope? {height: "1.1rem"}:{height: "2.3rem", marginTop: -15, marginBottom: -10} } 
+        <button className={`btn btn-primary ${ props.icon === 'mail' ? "btn-primary": props.icon === 'scope' ? "btn-secondary": "btn-danger" }`} type="button"
+            onClick={()=> props.icon=== 'whatsapp' ? openWhatsApp() : props.someFunction()}
+        >
+            <span> {props.text} </span>
+            <span className="">
+                <img style={currentIcon=== Mail? {height: "0.8rem"}: currentIcon=== ScopeSVG? {height: "1.1rem"}:{height: "2.3rem", marginTop: -15, marginBottom: -10} } 
                     src={currentIcon} alt="icono mail" 
                 />
             </span>

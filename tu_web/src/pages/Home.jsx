1d48dcf705e5ef07a1c9637915ce8c931cbe623d
carlosp-11 <carlosponce11@gmail.com>
2025-01-06 01:48:47 +0000

    feat: including properties jumbotron

--- tu_web/src/pages/Home.jsx
+++ tu_web/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import { ServicesJumbotron } from "../components/ServicesJumbotron";
 import { FinalJumbotron } from "../components/FinalJumbotron";
 import { FirstJumbotron } from "../components/Firstjumbotron";
 import { PresentingJumbotron } from "../components/PresentingJumbotron";
+import { PropertiesJumbotron } from "../components/PropertiesJumbotron";
 
 export const Home = () => {
     return (
         <div className="">
             <FirstJumbotron/>
             <PresentingJumbotron/>
+            <PropertiesJumbotron/>
             <ServicesJumbotron/>
             <ReviewCarrousel/>
             <FinalJumbotron/>

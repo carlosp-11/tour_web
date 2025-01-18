7484ca4e4876f9613fdff6d2a8175d1bc362ac88
carlosp-11 <carlosponce11@gmail.com>
2025-01-18 23:52:48 +0000

    fix: fonts on first jumbotron

--- tu_web/src/components/Firstjumbotron.jsx
+++ tu_web/src/components/Firstjumbotron.jsx
@@ -6,47 +6,52 @@ import GreenPicture from '../assets/pictures/green-picture.png';
 
 export const FirstJumbotron =()=>{
     const [isLargeScreen, setIsLargeScreen] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsLargeScreen(window.innerWidth >= 992); // Bootstrap 'lg' breakpoint (≥992px)
-        };
-
-        // Inicializar y escuchar cambios en el tamaño de la ventana
-        handleResize();
-        window.addEventListener("resize", handleResize);
-
-        // Limpieza del evento al desmontar
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    
+     useEffect(() => {
+                const handleResize = () => {
+                    setIsLargeScreen(window.innerWidth >= 992); // Bootstrap 'lg' breakpoint (≥992px)
+                };
+        
+                // Inicializar y escuchar cambios en el tamaño de la ventana
+                handleResize();
+                window.addEventListener("resize", handleResize);
+        
+                // Limpieza del evento al desmontar
+                return () => {
+                    window.removeEventListener("resize", handleResize);
+                };
+            }, []);
 
     return (
-         <div className='py-5 d-flex align-items-center' style={{
-                    backgroundColor: "#efefef",
-                    backgroundImage: `url(${GreenPicture})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    maxHeight: "91vh",
-                    //width: "100%",
-                }}>
-                    <div className='row'>
-                        <div className='col-12 text-center text-white'>
-                            <h2>Descubre tu Hogar en Tenerife </h2>
-                            <h6 className={`${!isLargeScreen? 'd-none': ''}`} > Encuentra el lugar perfecto para alguilar o comprar, adaptado a tus necesidades. </h6>
-                        </div>
-                        <div className='col-12 col-lg-8 ms-auto px-5'>
-                            <SimpleSearchbar/>
-                        </div>
-                        <div className={`col-6 d-flex align-items-center justify-content-center ${isLargeScreen? 'd-none': ''}`}>
-                            <ButtonImage text= "Contacto" icon="mail"/>
-                        </div>
-                        <div className={`col-6 col-lg-2 d-flex ${isLargeScreen? 'justify-content-end': 'justify-content-center'} pe-4`}>
-                            <WhatsAppButton/>
-                        </div>
-                    </div>
+         <div className='py-5 d-flex align-items-center' 
+            style={{
+                backgroundColor: "#efefef",
+                backgroundImage: `url(${GreenPicture})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                maxHeight: "91vh",
+                height: isLargeScreen? "85vh": "",
+                //width: "100%",
+            }}
+        >
+            <div className='row'>
+                <div className='col-12 text-center text-white'>
+                    <h1 className='raleway-bold'>Descubre Tu <br/> Hogar en Tenerife </h1>
+                    <h6 className='nunito-light small' style={!isLargeScreen? {display:'none'}: {}}> 
+                        Encuentra el lugar perfecto para alquilar o comprar, adaptado a tus necesidades. 
+                    </h6>
+                </div>
+                <div className='col-11 col-lg-8 mx-auto'>
+                    <SimpleSearchbar/>
+                </div>
+                <div className='col-12 text-center'>
+                    <span style={isLargeScreen? {display:'none'}: {}}>
+                        <ButtonImage text= "Contacto" icon="mail"/>
+                    </span>
+                    <WhatsAppButton/>
+                </div>
+            </div>
         </div>
     );
 }

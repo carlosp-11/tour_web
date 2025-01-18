87431f5bdbbdc8a49fdbbfdf10f11206774df6f3
carlosp-11 <carlosponce11@gmail.com>
2025-01-18 23:53:25 +0000

    fix: fonts on review card

--- tu_web/src/components/ReviewCard.jsx
+++ tu_web/src/components/ReviewCard.jsx
@@ -4,18 +4,18 @@ import Photo from '../assets/pictures/review-pic.png'
 export const ReviewCard =(props)=> {
     const firstData= {
         name:"José", 
-        date: "Agosto 2024",
+        date: "AGOSTO 2024",
         text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat lacus a purus rhoncus pellentesque eu nec mauris. Maecenas at lacus nunc. Quisque pellentesque metus dui, in blandit nisl pulvinar quis. Sed eget erat rhoncus, commodo dui sit amet, sagittis arcu."
     };
     const secondData= {
         name:"María", 
-        date: "Diciembre 2024",
+        date: "DICIEMBRE 2024",
         text:"Integer condimentum vestibulum odio at mattis. Donec pharetra sem in porttitor mattis. Nam pellentesque nunc in dignissim tempor. Nullam pharetra posuere varius. Proin vitae magna eu erat facilisis interdum. ", 
     };
     
     const thirdData= {
         name:"Jesús", 
-        date: "Junio 2024",
+        date: "JUNIO 2024",
         text:"Cras facilisis libero sem, id bibendum mauris dictum quis. Nunc ac malesuada est. Morbi mollis ipsum vel eros hendrerit, quis fermentum orci porta. In elementum risus quis justo luctus, quis lobortis est vestibulum. Vivamus vestibulum enim lacus. Donec interdum volutpat vulputate. Fusce maximus fermentum lorem et scelerisque. Aliquam porta lectus. ", 
     };
 
@@ -28,14 +28,14 @@ export const ReviewCard =(props)=> {
     }, [props.number]);
 
     return(
-        <div className="card mx-5 bg-secondary rounded-5" style={{height: "30rem"}}>
+        <div className="card mx-5 bg-secondary rounded-5" style={{height: "25rem"}}>
             <div className="card-body d-flex row align-items-center">
-                <p className="card-text align-self-end">{cardData.text} </p>
+                <p className="card-text align-self-end fw-light">{cardData.text} </p>
                 <div className="d-flex justify-content-center align-self-end">
                     <img src={Photo} alt="foto de usuario" style={{height: "4rem"}} className=""/>
                     <div className="ms-3 text-start">
-                        <h3 className="card-title">{cardData.name}</h3>
-                        <p className="card-text">{cardData.date} </p>
+                        <h3 className="card-title raleway-bold p-0 m-0">{cardData.name}</h3>
+                        <p className="card-text text-warning p-0 m-0 small nunito-light" style={{fontSize: 12}}>{cardData.date} </p>
                     </div>
                 </div>
             </div>

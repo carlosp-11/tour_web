4478d54f2297bd6a8c65688f574ffcbf0c0260f3
carlosp-11 <carlosponce11@gmail.com>
2025-01-20 01:28:02 +0000

    feat: including new properties carrousel

--- tu_web/src/components/PropertiesJumbotron.jsx
+++ tu_web/src/components/PropertiesJumbotron.jsx
@@ -1,27 +1,22 @@
 import { PillsComponent } from "./PillsComponent";
-import { PropertiesCard } from "./PropertiesCard";
+import { PropertiesCarrousel } from "./PropertiesCarrousel";
 
 export const PropertiesJumbotron =()=> {
     return (
         <div>
             <div className="row py-5">
-                <h1>Propiedades destacadas</h1>
-                <div className="col-12 col-lg-8">
+                <h2 className="col-12 raleway-bold px-5">Propiedades destacadas</h2>
+                <h6 className="col-12 nunito-light tu-font px-5 d-none d-lg-block">Viviendas amuebladas y acogedoras para estadías de corta y mediana duración.</h6>
+                <div className="col-12 col-lg-9 px-3">
                     <PillsComponent/>
                 </div>
-                <div className="col-4">
-                    <p>Ver todos los inmuebles</p>
+                <div className="col-3 d-flex align-items-center order-4 order-md-4 order-lg-0 justify-content-lg-end">
+                    <a href="#" className="nunito link-dark fw-bold">Ver todos los inmuebles</a>
                 </div>
                 <div className="row">
-                    <div className='col-12 col-lg-4'>
-                        <PropertiesCard/>
-                    </div>
-                    <div className='col-12 col-lg-4'>
-                        <PropertiesCard/>
-                    </div>
-                    <div className='col-12 col-lg-4'>
-                        <PropertiesCard/>
-                    </div>
+                        <div className="col-12">
+                            <PropertiesCarrousel />
+                        </div>
                 </div>
             </div>
         </div>

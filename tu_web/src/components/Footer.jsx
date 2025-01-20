a9ef33df66be815e7ad905180ccf69f467396c32
carlosp-11 <carlosponce11@gmail.com>
2025-01-20 01:26:15 +0000

    feat: colors, icons, fonts etc on footer

--- tu_web/src/components/Footer.jsx
+++ tu_web/src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ContactForm } from './ContactForm';
 import Logo from '../assets/logos/footer-tu-logo.png';
 import UpArrow from '../assets/icons/go-to-top.png';
 import WhiteUpArrow from '../assets/icons/white-up-arrow.png';
+import PhoneIconSVG from '../assets/svg/phone-solid.svg';
+import MailIconSVG from '../assets/svg/mail-envelope.svg';
+import FacebookIconSVG from '../assets/svg/facebook-icon.svg';
+import InstagramIconSVG from '../assets/svg/instagram-icon.svg';
+import LinkedInIconSVG from '../assets/svg/linkedin-icon.svg';
 
 export const Footer = () => {
 
@@ -46,73 +52,49 @@ export const Footer = () => {
                 <div className="col-12 col-lg-3 mb-3 order-1 d-flex justify-content-center pb-3">
                     <img src={Logo} alt='logo turismo urbano' style={{height:'11rem'}} className=""/>
                 </div>
-                <div className="col-6 col-lg-2 mb-3 order-3 order-md-3 pt-5 ps-5">
+                <div className="col-6 col-lg-2 mb-3 order-3 order-md-3 pt-5 ps-5 raleway">
                     <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-5">Comprar</a></li>
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-2">Alquileres</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-5 fw-semibold">Comprar</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-2 fw-semibold">Alquileres</a></li>
                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-6">Corta Temporada</a></li>
                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-6">Larga Temporada</a></li>
                     </ul>
                 </div>
-                <div className="col-6 col-lg-2 mb-3 order-3 order-md-3 pt-5 ps-5">
+                <div className="col-6 col-lg-2 mb-3 order-3 order-md-3 pt-5 ps-5 raleway">
                     <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-5">Vender</a></li>
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-2">Servicios</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-5 fw-semibold">Vender</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-5 mb-2 fw-semibold">Servicios</a></li>
                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-6">Asesoría</a></li>
                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-6">Gestión</a></li>
                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white fs-6">Seguros</a></li>
                     </ul>
                 </div>
                 <div className="col-12 col-lg-4 mb-3 order-2 order-md-2 order-lg-3 order-2">
-                    <h5>¿TIENES ALGUNA PREGUNTA?</h5>
-                    <h5>CONTÁCTANOS Y HAZ REALIDAD TU ESTANCIA IDEAL</h5>
-                    <form className="row g-2">
-                        <div className="form col-md-6">
-                            <input type="text" className="form-control bg-light border border-0 text-primary" id="name" placeholder='Nombre'/>
-                        </div>
-                        <div className="form col-md-6">
-                            <input type="email" className="form-control bg-light border border-0" id="email" placeholder='Email'/>
-                        </div>
-                        <div className="form col-md-6">
-                            <input type="text" className="form-control bg-light border border-0" id="phone" placeholder='Teléfono'/>
-                        </div>
-                        <div className="col-md-6">
-                            <select id="inputState" className="form-select bg-light border border-0" defaultValue="Quiero alquilar">
-                                <option>Quiero alquilar</option>
-                                <option>Quiero comprar</option>
-                                <option>Quiero vender</option>
-                                <option>Quiero información</option>
-                            </select>
-                        </div>
-                        <div className="form col-12">
-                            <textarea type="text" className="form-control bg-light border border-0" id="message" placeholder='Mensaje' style={{height: 120, resize: "none"}} />
-                        </div>
-                        <div className="col-12">
-                            <button type="submit" className="btn btn-info w-100 mb-3">Enviar</button>
-                            <p className='text-center' style={{fontSize: '0.9rem'}}>
-                                Al hacer click en Enviar aceptas nuestras {' '}
-                                <a className='link-light link-underline-light' onClick={()=>navigate('politica-privacidad')} role='button'> 
-                                    Políticas de privacidad
-                                </a>
-                            </p>
-                        </div>
-                    </form>
+                    <h5 className='raleway spaced-letter'>¿TIENES ALGUNA PREGUNTA?</h5>
+                    <h5 className='raleway spaced-letter'>CONTÁCTANOS Y HAZ</h5>
+                    <h5 className='raleway spaced-letter'>REALIDAD TU ESTANCIA IDEAL</h5>
+                    <ContactForm/>
                 </div>
             </div>
-            <div className="row" style={{marginTop: isLargeScreen ? -80 : "initial",}}>
-                <div className="col-10 col-lg-3 mb-3 ps-5 order-md-3 order-3" style={{marginTop: isLargeScreen ? -100 : "initial", paddingLeft:  isLargeScreen ? 50: "initial",}}>
-                    <p>Dirección:</p>
+            <div className="row" style={{marginTop: isLargeScreen ? -80 : "initial", letterSpacing: 2}}>
+                <div className="col-10 col-lg-3 mb-3 ps-5 order-md-3 order-3 raleway" style={{marginTop: isLargeScreen ? -100 : "initial", paddingLeft:  isLargeScreen ? 50: "initial",}}>
+                    <p className='fw-semibold'>Dirección:</p>
                     <p>Calle Nuestra Señora de la Encarnación 1, local Bajo 2, Valle San Lorenzo, 38262, Arona, Tenerife, España</p>
-                    <span> <p>+34 608 37 37 97</p> </span>
-                    <span> <p>info@turismourbano.org</p> </span>
+                    <span className=''> <p> <img src={PhoneIconSVG} /> +34 608 37 37 97</p> </span>
+                    <span> <p> <img src={MailIconSVG} /> info@turismourbano.org</p> </span>
                 </div>
 
-                <div className="col-12 col-lg-2 mb-3 ps-5 order-1 order-md-1 order-lg-3">
-                    <a href="#" className="nav-link p-0 text-white fs-5 mb-5">Nosotros</a>
+                <div className="col-12 col-lg-2 mb-3 ps-5 order-1 order-md-1 order-lg-3 raleway">
+                    <a href="#" className="nav-link p-0 text-white fs-5 mb-5 fw-semibold">Nosotros</a>
                 </div>
 
-                <div className="col-12 col-lg-2 mb-3 ps-5 order-2 order-md-2 order-lg-3 mb-5">
-                    <h5 className="" >Síguenos</h5>
+                <div className="col-12 col-lg-2 mb-3 ps-5 order-2 order-md-2 order-lg-3 mb-5 raleway row">
+                    <h5 className="fw-semibold col-12 pb-0 mb-0" >Síguenos</h5>
+                    <span className='col-12 d-flex'>
+                        <a href="#" className='pe-2 link-underline link-underline-opacity-0'><img src={FacebookIconSVG} /> </a>
+                        <a href="#" className='px-2 link-underline link-underline-opacity-0'><img src={InstagramIconSVG} /> </a>
+                        <a href="#" className='px-2 link-underline link-underline-opacity-0'><img src={LinkedInIconSVG} /> </a>
+                    </span>
                 </div>
                 <div className="col-12 col-lg-1 mb-3 d-flex justify-content-center ms-auto order-md-4 order-lg-3 order-4" 
                     style={{marginTop: isLargeScreen ? -20 : "initial",}}
@@ -126,16 +108,17 @@ export const Footer = () => {
                     </div>
                 </div>
             </div>
+        
 
             <div className="row pt-3 mt-5 border-top mx-4">
                 <div className='col-12 col-lg-4 text-center text-lg-start'> 
-                    <p> Copyright  &copy; {getCurrentYear()} Turismo URBANO</p>
+                    <p className='nunito-compact small fw-light'> Copyright  &copy; {getCurrentYear()} <span className='fw-bold'> Turismo URBANO </span></p>
                 </div>
                 <div className='col-12 col-lg-8 ms-auto d-flex justify-content-center justify-content-lg-end'> 
                     <ul className="list-unstyled d-flex">
-                        <li className="ms-3"><a className='link-light link-underline-light' onClick={()=>navigate('aviso-legal')} role='button'>Aviso Legal</a></li>
-                        <li className="ms-3"><a className='link-light link-underline-light' onClick={()=>navigate('politica-cookies')} role='button'>Política de Cookies</a></li>
-                        <li className="ms-3"><a className='link-light link-underline-light' onClick={()=>navigate('politica-privacidad')} role='button'>Política de privacidad</a></li>
+                        <li className="px-2 border-end"><a className='link-light link-underline-light nunito-compact small fw-light' onClick={()=>navigate('aviso-legal')} role='button'>Aviso Legal</a></li>
+                        <li className="px-2 border-end"><a className='link-light link-underline-light nunito-compact small fw-light' onClick={()=>navigate('politica-cookies')} role='button'>Política de Cookies</a></li>
+                        <li className="ms-2"><a className='link-light link-underline-light nunito-compact small fw-light' onClick={()=>navigate('politica-privacidad')} role='button'>Política de privacidad</a></li>
                     </ul>
                 </div>
             </div>

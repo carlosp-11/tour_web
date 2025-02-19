fe3e310be7cc0601321d1c6e8e1edf9eeaf8c3c5
carlosp-11 <carlosponce11@gmail.com>
2025-02-19 20:57:09 +0000

    feat: header styling

--- tu_web/src/components/HeaderComponent.jsx
+++ tu_web/src/components/HeaderComponent.jsx
@@ -1,8 +1,9 @@
 export const HeaderComponent = (props) => {
     return (
-        <div className="w-100 container-fluid mx-0 px-0">
-            <img src={props.image} alt="header" style={{width:'100%', height: '30vh', objectFit:'cover' }}/>
-            <p className="raleway ps-5 py-3" style={{fontSize: 12}}>Inicio /  <span className="fw-bold">{props.tag}</span> </p>
+        <div className="w-100 container-fluid mx-0 px-0 position-relative">
+            <img src={props.image} alt="header" className="" style={{width:'100%', height: '30vh', objectFit:'cover' }}/>
+            <p className="raleway ps-4 py-3 d-none d-lg-block" style={{fontSize: 12}}>Inicio /  <span className="fw-bold">{props.tag}</span> </p>
+            <p className="raleway-bold position-absolute fs-1 text-white top-50 start-50 translate-middle d-lg-none" style={{top: 100,}}> {props.tag} </p>
         </div>
     );
 }

d3473115705828386acde7aeb5cdd21c24334607
carlosp-11 <carlosponce11@gmail.com>
2025-02-19 20:57:58 +0000

    feat: spinner

---
+++ tu_web/src/components/SpinnerComponent.jsx
@@ -0,0 +1,13 @@
+export const SpinnerComponent = () => (
+    <div className="text-center py-5">
+        <div className="spinner-grow mx-3" role="status" style={{color:'#0089ad'}}>
+            <span className="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning mx-3" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+        <div className="spinner-grow text-success mx-3" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>
+)
